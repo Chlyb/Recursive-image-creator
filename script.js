c4f7f29bd940a640a4dade8271436c0d94230fcc
Tomasz Ociepka <tomipl200@o2.pl>
2019-10-09 18:37:56 +0200

    Interactive scaling

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const image = new Image();
+
+const clipCanvas = document.createElement('canvas');
+const clipCtx = clipCanvas.getContext('2d');
+var clipPoints = [];
+
 image.onload = onLoad;
 //image.src = 'img.jpg';
 image.src = 'hulkhogan2.jpg';
-
-var clipPoints = [];
 
 var x = 0;
 var y = 0;
@@ -13,12 +16,14 @@ var y = 0;
 var dx = 0;
 var dy = 0;
 
-//scale
-var sx = 1.0;
-var sy = 1.0;
+var scale = 1;
 
-var clip_x = 0;
-var clip_y = 0;
+//clip middle
+var clipMid_x = 0;
+var clipMid_y = 0;
+
+var controller_x;
+var controller_y;
 
 var dragged = null;
 var createPoints = true;
@@ -27,29 +32,23 @@ canvas.addEventListener('mousedown', e => {
 	x = e.clientX;
 	y = e.clientY;
 
-	for (let p of clipPoints) {
-		let d = (p[0] - x) ** 2 + (p[1] - y) ** 2;
-		if (d < 169) {
-			dragged = p;
-			break;
+	if ((controller_x - x) ** 2 + (controller_y - y) ** 2 < 169) {
+		dragged = "controller";
+	}
+
+	if (dragged === null) {
+		for (let p of clipPoints) {
+			let d = (p[0] - x) ** 2 + (p[1] - y) ** 2;
+			if (d < 169) {
+				dragged = p;
+				break;
+			}
 		}
 	}
 
 	if (dragged === null && createPoints) {
 		clipPoints.push([x, y]);
-
-		clip_x = 0;
-		clip_y = 0;
-		for (let p of clipPoints) {
-			clip_x += p[0];
-			clip_y += p[1];
-		}
-
-		clip_x /= clipPoints.length;
-		clip_y /= clipPoints.length;
-
-		dx = clip_x;
-		dy = clip_y;
+		updateClipMiddle();
 	}
 	drawRecursive();
 });
@@ -60,8 +59,24 @@ canvas.addEventListener('mouseup', e => {
 
 canvas.addEventListener('mousemove', e => {
 	if (dragged !== null) {
-		dragged[0] = e.clientX;
-		dragged[1] = e.clientY;
+		if (dragged == "controller") {
+
+      //default distance between clip middle and controller
+			let d0 = (clipPoints[0][0] - clipMid_x) ** 2 + (clipPoints[0][1] - clipMid_y) ** 2;
+      //new distance between clip middle and controller
+			let d = (clipMid_x + dx - e.clientX) ** 2 + (clipMid_y + dy - e.clientY) ** 2;
+
+			d0 = Math.sqrt(d0);
+			d = Math.sqrt(d);
+
+      scale = d/d0;
+
+		} else {
+			dragged[0] = e.clientX;
+			dragged[1] = e.clientY;
+
+      updateClipMiddle();
+		}
 	} else if (e.buttons > 0) {
 		dx += e.clientX - x;
 		dy += e.clientY - y;
@@ -73,11 +88,11 @@ canvas.addEventListener('mousemove', e => {
 
 document.addEventListener("keydown", e => {
 	if (e.keyCode == 87) {
-		sx *= 1.01;
-		sy *= 1.01;
+		scale *= 1.04;
+		scale *= 1.04;
 	} else if (e.keyCode == 83) {
-		sx *= 0.99;
-		sy *= 0.99;
+		scale *= 0.96;
+		scale *= 0.96;
 	} else {
 		createPoints = false;
 		console.log("koniec");
@@ -86,48 +101,37 @@ document.addEventListener("keydown", e => {
 });
 
 function drawRecursive() {
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+	clipCtx.clearRect(0, 0, clipCanvas.width, clipCanvas.height);
+	clipCtx.save();
+
+	if (clipPoints.length > 0) {
+		clipCtx.beginPath();
+		clipCtx.moveTo(clipPoints[0][0], clipPoints[0][1]);
+		for (let p of clipPoints) {
+			clipCtx.lineTo(p[0], p[1]);
+		}
+		clipCtx.closePath();
+	}
+	clipCtx.clip();
+
+	clipCtx.drawImage(image, 0, 0);
 	ctx.drawImage(image, 0, 0);
 
-	ctx.setTransform(1, 0, 0, 1, 0, 0);
-	//ctx.rotate(6 * Math.PI / 180);
-
-	let actual_sx = 1;
-	let actual_sy = 1;
-
-	let rel_x = 0;
-	let rel_y = 0;
+	ctx.translate(clipMid_x, clipMid_y);
 
 	for (let i = 0; i < 10; i++) {
-    ctx.scale(sx, sy);
-		actual_sx *= sx;
-		actual_sy *= sy;
+		ctx.translate(dx, dy);
 
-		if (i > 0) {
-			rel_x += (dx - clip_x) * sx ** i;
-			rel_y += (dy - clip_y) * sy ** i;
-		}
+		ctx.scale(scale, scale);
 
-		let off_x = dx / actual_sx + rel_x / actual_sx - clip_x;
-		let off_y = dy / actual_sy + rel_y / actual_sy - clip_y;
-
-		if (clipPoints.length > 0) {
-			ctx.beginPath();
-			ctx.moveTo(clipPoints[0][0] + off_x, clipPoints[0][1] + off_y);
-			for (let p of clipPoints) {
-				ctx.lineTo(p[0] + off_x, p[1] + off_y);
-			}
-			ctx.closePath();
-		}
-    
-		ctx.restore();
-		ctx.save();
-		ctx.setTransform(1, 0, 0, 1, 0, 0);
-		ctx.scale(actual_sx, actual_sy);
-		ctx.clip();
-
-		ctx.drawImage(image, off_x, off_y);
+		ctx.translate(-clipMid_x, -clipMid_y);
+		ctx.drawImage(clipCanvas, 0, 0);
+		ctx.translate(clipMid_x, clipMid_y);
 	}
-	ctx.restore();
+
+	clipCtx.restore();
 	ctx.setTransform(1, 0, 0, 1, 0, 0);
 	drawGUI();
 }
@@ -145,25 +149,51 @@ function drawGUI() {
 	ctx.closePath();
 	ctx.stroke();
 
-	ctx.fillStyle = 'black';
+	ctx.fillStyle = 'white';
+	ctx.strokeStyle = 'black';
 	for (let p of clipPoints) {
 		ctx.beginPath()
 		ctx.arc(p[0], p[1], 7, 0, Math.PI * 2, false);
 		ctx.closePath();
 		ctx.fill();
+		ctx.stroke();
 	}
 
-	ctx.fillStyle = 'white';
-	for (let p of clipPoints) {
-		ctx.beginPath()
-		ctx.arc(p[0], p[1], 5, 0, Math.PI * 2, false);
-		ctx.closePath();
-		ctx.fill();
-	}
+	let rel_x = clipPoints[0][0] - clipMid_x;
+	rel_x *= scale;
+
+	let rel_y = clipPoints[0][1] - clipMid_y;
+	rel_y *= scale;
+
+	controller_x = rel_x + clipMid_x + dx;
+	controller_y = rel_y + clipMid_y + dy;
+
+	ctx.fillStyle = 'red';
+	ctx.beginPath();
+	ctx.arc(controller_x, controller_y, 7, 0, Math.PI * 2, false);
+	ctx.closePath();
+	ctx.fill();
+	ctx.stroke();
 }
 
 function onLoad() {
-  canvas.width = this.naturalWidth;
+	canvas.width = this.naturalWidth;
 	canvas.height = this.naturalHeight;
-  drawRecursive();
+	clipCanvas.width = this.naturalWidth;
+	clipCanvas.height = this.naturalHeight;
+	drawRecursive();
+}
+
+function updateClipMiddle(){
+  if(clipPoints.length == 0) return;
+
+  clipMid_x = 0;
+	clipMid_y = 0;
+	for (let p of clipPoints) {
+		clipMid_x += p[0];
+		clipMid_y += p[1];
+	}
+
+	clipMid_x /= clipPoints.length;
+	clipMid_y /= clipPoints.length;
 }
